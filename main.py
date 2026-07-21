2dfa40a2624f5424fef2ddb2acf985a419af09f6
contary <contary295@gmail.com>
2026-07-21 11:25:48 +0300

    B: Discord login ok

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-def main():
-    print("Hello from cutie-bot!")
+import os
+import discord
+from dotenv import load_dotenv
 
+load_dotenv()
 
-if __name__ == "__main__":
-    main()
+api_key = os.environ.get("DISCORD_TOKEN")
+if not api_key :
+    raise RuntimeError("Api key not found")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content == "Ping":
+        await message.channel.send('Pong!')
+
+client.run(api_key)
+
