ce41a967de68a81a65512b1e4234a273a1e42d3a
contary <contary295@gmail.com>
2026-07-21 11:51:34 +0300

    B: Added safe shutdown option. Added grabbing commonly useful IDs from ID.py. ID.py is in .gitignore.

---
+++ README.md
@@ -0,0 +1,3 @@
+Kissu's bot for personal use.
+.env and ID files are not included for secutiry reasons.
+ID.py is meant to have a couple of common useful IDs such as OWNER_ID.
--- main.py
+++ main.py
@@ -11,6 +11,8 @@ if not api_key :
 intents = discord.Intents.default()
 intents.message_content = True
 
+from ID import *
+
 client = discord.Client(intents=intents)
 
 @client.event
@@ -21,8 +23,14 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
+    print(f"Message:\n{message.content}\nSent by user:\n{message.author.name}, ID {message.author.id}")
     if message.content == "Ping":
         await message.channel.send('Pong!')
+    if message.author.id == OWNER_ID :
+        if message.content == "Shutdown" :
+            await message.channel.send("Shutting down...")
+            await client.close()
+
 
 client.run(api_key)
 
