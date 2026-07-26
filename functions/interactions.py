a3d77b24971eb5d6df5262e0fa6fa19d8ecacc13
contary <contary295@gmail.com>
2026-07-26 19:29:56 +0300

    H: updated interactions.get_info to return a proper error explanation instead of throwing the raw exception info as a message.

--- functions/interactions.py
+++ functions/interactions.py
@@ -1,13 +1,19 @@
 from classes.user import User
 
 async def hug(client, message, content) :
-    [id1, name1, id2, name2] = await getinfo(client, message, content)
+    info = await getinfo(client, message, content)
+    if not info :
+        return 0
+    [id1, name1, id2, name2] = info
     user1 = User(id1, name1)
     user2 = User(id2, name2)
     await user1.hug(user2, message)
     
 async def kiss(client, message, content) :
-    [id1, name1, id2, name2] = await getinfo(client, message, content)
+    info = await getinfo(client, message, content)
+    if not info :
+        return 0
+    [id1, name1, id2, name2] = info
     user1 = User(id1, name1)
     user2 = User(id2, name2)
     await user1.kiss(user2, message)
@@ -16,12 +22,13 @@ async def getinfo(client, message, content) :
     id1 = message.author.id
     name1 = message.author.display_name
     user1 = User(id1, name1)
+    if content in user1.alias :
+        content = user1.alias[content]
     try :
-        if content in user1.alias :
-            content = user1.alias[content]
         id2 = content.split("<@")[1].split(">")[0]
-        info2 = await client.fetch_user(id2)
-        name2 = info2.display_name  
-        return id1, name1, id2, name2
     except Exception as e:
-        await message.channel.send(f"Error: {e}")
+        await message.channel.send("Could not find this user! Correctly mention a user or use alias.")
+        return 0
+    info2 = await client.fetch_user(id2)
+    name2 = info2.display_name  
+    return id1, name1, id2, name2
