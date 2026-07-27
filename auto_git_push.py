dad4a0dfe030232add4d84cbee9fc77565a7717c
contary <contary295@gmail.com>
2026-07-27 12:06:14 +0300

    Auto-commit on bot shutdown

---
+++ auto_git_push.py
@@ -0,0 +1,13 @@
+import subprocess
+async def sync_to_github(message):
+    try:
+        await message.channel.send("Uploading to GitHub...")
+        subprocess.run(["git", "add", "."], check=True)
+        commit_message = "Auto-commit on bot shutdown"
+        subprocess.run(["git", "commit", "-m", commit_message], check=True)
+        subprocess.run(["git", "push"], check=True, stdout=subprocess.DEVNULL)
+        await message.channel.send("Uploaded successfully!")
+        return True
+    except subprocess.CalledProcessError as e:
+        message.channel.send(f"Git sync failed with error: {e}")
+        return False
--- functions/shutdown.py
+++ functions/shutdown.py
@@ -1,6 +1,8 @@
 from shared.ID import OWNER_ID
+from auto_git_push import sync_to_github
 async def shutdown(client, message) :
     if message.author.id == OWNER_ID :
+        await sync_to_github(message)
         await message.channel.send("Shutting down...")
         await client.close()
     else :
