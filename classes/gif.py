ccf0c79ad25154d08b09d37a1ea4940b7f01a401
contary <contary295@gmail.com>
2026-07-28 12:02:39 +0300

    Auto-commit on bot shutdown

--- classes/gif.py
+++ classes/gif.py
@@ -1,12 +1,14 @@
 import json
 import discord
+import asyncio
 
 class Gif:
-    def __init__(self, message, type, url) :
+    def __init__(self, client, message, type, url) :
         self.url = url
         self.type = type
         self.gifs = self.read_gifs()
         self.message = message
+        self.client = client
 
     def read_gifs(self) :
         with open('shared/gifs.json', 'r+') as f:
@@ -25,6 +27,25 @@ class Gif:
         await bot_message.add_reaction("✅")
         await bot_message.add_reaction("❌")
 
-    async def add(self) :
+        def reaction_check(reaction, user): #checking if it's the same message, same person, and a valid reaction
+            return (
+                user == self.message.author and reaction.message.id == self.message.id 
+                and str(reaction.emoji) in ["✅", "❌"]
+            )
+        try :
+            reaction, user = await self.client.wait_for('reaction_add', timeout=15.0, check=reaction_check)
+        except asyncio.TimeoutError:
+            await self.message.channel.send("Action declined - no confirmation received.")
+        else :
+            if str(reaction.emoji) == "✅" :
+                try :
+                    self.add()
+                    await self.message.channel.send("Added successfully!")
+                except Exception as e:
+                    await self.message.channel.send(f"Failed to add with error: {e}")
+            if str(reaction.emoji) == "❌" :
+                await self.message.channel.send("Action declined by user.")
+
+    def add(self) :
         pass
     
--- functions/gifs.py
+++ functions/gifs.py
@@ -7,16 +7,13 @@ async def gif(client, message, content):
     if not called :
         await message.channel.send("Available functions: gif add, gif remove")
 
-async def gif_add(message, content):
+async def gif_add(client, message, content):
     args = content.split(' ', maxsplit=1)
     if not len(args) == 2 :
         return await message.channel.send("Invalid arguments! Usage: gif add gif_type your_gif_url_here")
     [type, url] = args
-    gif = Gif(message, type, url)
+    gif = Gif(client, message, type, url)
     await gif.check()
-    if gif_add() :
-        return await message.channel.send("Added successfully! (Kidding, just a check)")
-    return await message.channel.send("Something went wrong.")
 
 async def gif_remove(message, content):
     pass
