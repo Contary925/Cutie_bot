5ba9dffebb40a19f4d7ac5e01f47108fe7e5b7cf
contary <contary295@gmail.com>
2026-07-29 16:56:44 +0300

    Auto-commit on bot shutdown

--- functions/help.py
+++ functions/help.py
@@ -2,9 +2,9 @@ async def help(message) :
     text = """  Hello! I am Cutie Bot, Kissu's bot for personal use made with discord.py. 
 The prefix to use me is '**uwu**' (or '**уву**') - use it every time you want to call me!
 Here are some of my commands:
-**Interactions**: hug, kiss, pat, bite
+**Interactions**: hug, kiss, pat, bite, boop, lick
 **Alias**: alias add, alias remove, alias list
-**Gifs**: gif add, gif remove, gif list
-**Misc.**: ping, boop
-**Owner only**: shutdown, gitpush"""
+**Gifs**: gif add, gif remove, gif list (remove and add require administrator permissions)
+**Misc.**: ping
+**Owner only**: shutdown, gitpush, perms set"""
     await message.channel.send(text)
